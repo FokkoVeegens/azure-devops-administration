beee44fcc36e61477bcbf673cd7d227307671d2e
Fokko <24793348+FokkoVeegens@users.noreply.github.com>
2022-08-01 16:51:32 +0200

    Added properties for remote control of agents
    With this script I want to be able to re-register agents on a bulk-basis
    It produces a CSV that is input for a PS remoting script

--- azure-pipelines/export-agent-pools-and-agents.ps1
+++ azure-pipelines/export-agent-pools-and-agents.ps1
@@ -1,6 +1,9 @@
-$coll = https://tfsserver:8080/tfs/defaultcollection
-$apiurl = "$coll/_apis"
+$coll = "https://dev.azure.com/YOURORG"
 $filepath = "C:\temp\agentsandpools.csv"
+$pat = Get-Content -Path ".\pat.txt"
+$encodedPat = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes(":$pat"))
+$header = @{Authorization = "Basic $encodedPat"}
+$apiurl = "$coll/_apis"
  
 Class AgentPoolEntry {
     [int]$AgentPoolId
@@ -10,26 +13,44 @@ Class AgentPoolEntry {
     [bool]$AgentEnabled
     [string]$AgentStatus
     [string]$AgentVersion
+    [string]$AgentHomeDirectory
+    [string]$AgentUserDomain
+    [string]$AgentUserName
+    [string]$AgentComputerName
+    [string]$AgentOS
 }
  
 function Get-Agents ($poolid)
 {
-    $uri = "$apiurl/distributedtask/pools/$poolid/agents"
-    $response = invoke-webrequest -Uri $uri -Method GET -ContentType "application/json" -UseDefaultCredentials
+    $uri = "$apiurl/distributedtask/pools/$poolid/agents?includeCapabilities=true"
+    $response = invoke-webrequest -Uri $uri -Method GET -ContentType "application/json" -Headers $header
     $agents = ($response.Content | ConvertFrom-Json).value
     return $agents
 }
- 
-$uri = "$apiurl/distributedtask/pools"
-$response = invoke-webrequest -Uri $uri -Method GET -ContentType "application/json" -UseDefaultCredentials
-$agentpools = ($response.Content | ConvertFrom-Json).value
+
+function Get-Pools ()
+{
+    $uri = "$apiurl/distributedtask/pools"
+    $response = invoke-webrequest -Uri $uri -Method GET -ContentType "application/json" -Headers $header
+    $agentpools = ($response.Content | ConvertFrom-Json).value  
+    return $agentpools
+}
+
+$agentpools = Get-Pools
  
 $agentpoolentries = New-Object System.Collections.ArrayList
 foreach ($agentpool in $agentpools)
 {
+    Write-Host "Processing pool '$($agentpool.name)'"
+    if ($agentpool.isHosted)
+    {
+        Write-Host "Skipping pool, because it is Microsoft-hosted"
+        continue
+    }
     $agents = Get-Agents -poolid $agentpool.id
     foreach ($agent in $agents)
     {
+        Write-Host "  Processing agent '$($agent.name)'"
         $agentpoolentry = New-Object AgentPoolEntry
         $agentpoolentry.AgentPoolId = $agentpool.id
         $agentpoolentry.AgentPoolName = $agentpool.name
@@ -38,8 +59,15 @@ foreach ($agentpool in $agentpools)
         $agentpoolentry.AgentEnabled = $agent.enabled
         $agentpoolentry.AgentStatus = $agent.status
         $agentpoolentry.AgentVersion = $agent.version
-        $agentpoolentries.Add($agentpoolentry)
+        $agentpoolentry.AgentHomeDirectory = $agent.systemCapabilities.'Agent.HomeDirectory'
+        $agentpoolentry.AgentUserDomain = $agent.systemCapabilities.USERDOMAIN
+        $agentpoolentry.AgentUserName = $agent.systemCapabilities.USERNAME
+        $agentpoolentry.AgentComputerName = $agent.systemCapabilities.COMPUTERNAME
+        $agentpoolentry.AgentOS = $agent.systemCapabilities.'Agent.OS'
+        $agentpoolentries.Add($agentpoolentry) | Out-Null
     }
 }
- 
+
+Write-Host "Writing CSV file"
 $agentpoolentries | Export-Csv -Path $filepath -UseCulture
+Write-Host "Done"
