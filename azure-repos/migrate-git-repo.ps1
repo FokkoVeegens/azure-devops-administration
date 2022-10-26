d2b9c5f3eb0a0d571caf96446871c6bda393888b
Fokko <24793348+FokkoVeegens@users.noreply.github.com>
2022-10-26 15:35:28 +0200

    Create migrate-git-repo.ps1

---
+++ azure-repos/migrate-git-repo.ps1
@@ -0,0 +1,3 @@
+# Please check https://gist.github.com/Ba4bes/29afffc1a8708169e9f40b326b162ff3#file-invoke-azdorepomigration-ps1 for this
+# Usage is described in this blog: https://4bes.nl/2021/07/25/migrate-azure-devops-repos-with-powershell/
+# Pull requests cannot be migrated
